9f6532d4a0c044182046c8855ac1e61d0d5de915
Szymon Dabrowski <sdabrowski@openskydata.com>
2022-08-15 22:10:12 +0200

    init: initalize project structure & basic implementation of auth

---
+++ Clean.Application/Services/Auth/AuthService.cs
@@ -0,0 +1,26 @@
+﻿using Clean.Application.Services.Auth.Model;
+
+namespace Clean.Application.Services.Auth;
+
+public class AuthService : IAuthService
+{
+    public AuthResult Login(string email, string password)
+    {
+        return new AuthResult(
+            Guid.NewGuid(),
+            "firstName",
+            "lastName",
+            email,
+            "token");
+    }
+
+    public AuthResult Register(string firstName, string lastName, string email, string password)
+    {
+        return new AuthResult(
+            Guid.NewGuid(),
+            firstName,
+            lastName,
+            email,
+            "token");
+    }
+}
---
+++ Clean.Contracts/Auth/Requests/RegisterRequest.cs
@@ -0,0 +1,6 @@
+﻿namespace Clean.Contracts.Auth.Requests;
+public record RegisterRequest(
+    string FirstName,
+    string LastName,
+    string Email,
+    string Password);

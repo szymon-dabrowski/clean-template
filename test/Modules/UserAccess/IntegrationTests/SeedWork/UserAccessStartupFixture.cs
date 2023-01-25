33dc2f88bf5d4d96762fd14d7038dcb23b1e14bf
Szymon Dabrowski <szymon.dabrowski.05@gmail.com>
2023-01-25 08:31:00 +0100

    feature/user-access-tests - integration tests & unit test for useraccess module (#30)
    Co-authored-by: Szymon Dabrowski <sdabrowski@openskydata.com>

---
+++ test/Modules/UserAccess/IntegrationTests/SeedWork/UserAccessStartupFixture.cs
@@ -0,0 +1,46 @@
+﻿using Clean.Modules.Shared.Application.Interfaces.ExecutionContext;
+using Clean.Modules.UserAccess.Infrastructure.Module;
+using Clean.Modules.UserAccess.Infrastructure.Setup;
+using Clean.Modules.UserAccess.Infrastructure.Setup.Options;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Moq;
+
+namespace Clean.Modules.UserAccess.IntegrationTests.SeedWork;
+
+public sealed class UserAccessStartupFixture : IDisposable
+{
+    public UserAccessStartupFixture()
+    {
+        Dictionary<string, string?> configurationKeys = new()
+        {
+            { $"{JwtOptions.Jwt}:Audience", "user-access-tests-aud" },
+            { $"{JwtOptions.Jwt}:Issuer", "user-access-tests-iss" },
+            { $"{JwtOptions.Jwt}:Secret", "super-secret-key" },
+            { $"{JwtOptions.Jwt}:Expiration", "120" },
+        };
+
+        Configuration = new ConfigurationBuilder()
+            .AddInMemoryCollection(configurationKeys)
+            .Build();
+
+        UserAccesStartup.Initialize(
+            Configuration,
+            executionContextAccessor: Mock.Of<IExecutionContextAccessor>(),
+            options =>
+            {
+                options.UseInMemoryDatabase("useraccess_module");
+            });
+
+        UserAccessModule = new UserAccessModule(new UserAccessServiceProvider());
+    }
+
+    public UserAccessModule UserAccessModule { get; }
+
+    public IConfiguration Configuration { get; }
+
+    public void Dispose()
+    {
+        // cleanup
+    }
+}
---
+++ test/Modules/UserAccess/IntegrationTests/Users/UsersTests.cs
@@ -0,0 +1,79 @@
+﻿using Clean.Modules.UserAccess.Application.Users.Dto;
+using Clean.Modules.UserAccess.Application.Users.GetToken;
+using Clean.Modules.UserAccess.Application.Users.RegisterUser;
+using Clean.Modules.UserAccess.Infrastructure.Module;
+using Clean.Modules.UserAccess.Infrastructure.Setup.Options;
+using Clean.Modules.UserAccess.IntegrationTests.SeedWork;
+using FluentAssertions;
+using Microsoft.Extensions.Configuration;
+using System.IdentityModel.Tokens.Jwt;
+using Xunit;
+
+namespace Clean.Modules.UserAccess.IntegrationTests.Users;
+public class UsersTests : IClassFixture<UserAccessStartupFixture>
+{
+    private readonly UserAccessModule userAccessModule;
+
+    private readonly IConfiguration configuration;
+
+    public UsersTests(UserAccessStartupFixture userAccessStartupFixture)
+    {
+        userAccessModule = userAccessStartupFixture.UserAccessModule;
+        configuration = userAccessStartupFixture.Configuration;
+    }
+
+    [Fact]
+    public async Task CreateUser_UserValid_UserCreated()
+    {
+        var password = "P@ssw0rd";
+
+        var expectedAuthResult = new AuthResultDto(
+            Id: Guid.Empty,
+            FirstName: "FirstName",
+            LastName: "LastName",
+            Email: "email@domain.com",
+            Token: string.Empty);
+
+        var user = await userAccessModule.ExecuteCommand(new RegisterUserCommand(
+            expectedAuthResult.FirstName,
+            expectedAuthResult.LastName,
+            expectedAuthResult.Email,
+            password));
+
+        var authResult = await userAccessModule.ExecuteQuery(new GetTokenQuery(
+            expectedAuthResult.Email,
+            password));
+
+        user.IsError.Should().BeFalse();
+        authResult.IsError.Should().BeFalse();
+        authResult.Value.FirstName.Should().Be(expectedAuthResult.FirstName);
+        authResult.Value.LastName.Should().Be(expectedAuthResult.LastName);
+        authResult.Value.Email.Should().Be(expectedAuthResult.Email);
+        user.Value.Id.Should().Be(authResult.Value.Id);
+        AssertToken(user.Value);
+        AssertToken(authResult.Value);
+    }
+
+    private void AssertToken(AuthResultDto actual)
+    {
+        var jwt = new JwtSecurityTokenHandler().ReadJwtToken(actual.Token);
+
+        var audience = configuration.GetSection(JwtOptions.Jwt)["Audience"];
+        var issuer = configuration.GetSection(JwtOptions.Jwt)["Issuer"];
+
+        jwt.Claims.Should().ContainSingle(c =>
+            c.Type == JwtRegisteredClaimNames.Aud && c.Value == audience);
+
+        jwt.Claims.Should().ContainSingle(c =>
+            c.Type == JwtRegisteredClaimNames.Iss && c.Value == issuer);
+
+        jwt.Claims.Should().ContainSingle(c =>
+            c.Type == JwtRegisteredClaimNames.Sub && c.Value == actual.Id.ToString());
+
+        jwt.Claims.Should().ContainSingle(c =>
+            c.Type == JwtRegisteredClaimNames.GivenName && c.Value == actual.FirstName);
+
+        jwt.Claims.Should().ContainSingle(c =>
+            c.Type == JwtRegisteredClaimNames.FamilyName && c.Value == actual.LastName);
+    }
+}
